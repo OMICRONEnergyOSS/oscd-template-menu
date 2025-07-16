102b24bae09f3659e519fc04c5a203412d4b6f07
Stephen Renwick <stephen.renwick@omicronenergy.com>
2025-07-16 15:51:36 +0200

    feat: initial commit
    Release-As: 0.0.1
    
    Resolves: #1

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
+import fs from 'fs';
+
+const tsconfig = JSON.parse(fs.readFileSync('./tsconfig.json', 'utf8'));
+const demoTsconfig = {
+  ...tsconfig,
+  compilerOptions: { ...tsconfig.compilerOptions, outDir: 'dist/demo' },
+};
+
+export default [
+  {
+    input: 'oscd-menu-save.ts',
+    output: {
+      sourcemap: true, // Add source map to build output
+      format: 'es', // ES module type export
+      dir: 'dist', // The build output folder
+      // preserveModules: true,  // Keep directory structure and files
+    },
+    preserveEntrySignatures: 'strict', // leaves export of the plugin entry point
+
+    plugins: [
+      /** Resolve bare module imports */
+      nodeResolve(),
+      typescript(),
+    ],
+  },
+  {
+    input: 'demo/index.html',
+    plugins: [
+      html({
+        input: 'demo/index.html',
+        minify: true,
+      }),
+      nodeResolve(),
+      typescript(demoTsconfig),
+    ],
+    output: {
+      dir: 'dist/demo',
+      format: 'es',
+      sourcemap: true,
+    },
+  },
+];
