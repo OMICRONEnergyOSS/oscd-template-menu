ac86bb85862581c2522ce8a07f19d6adaa0cd2ad
Stephen Renwick <stephen.renwick@omicronenergy.com>
2025-07-23 14:22:27 +0200

    ci: updated demo + pkg.json scripts

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -10,9 +10,9 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   /** Use regular watch mode if HMR is not enabled. */
   watch: !hmr,
   /** Resolve bare module imports */
-  nodeResolve: {
-    exportConditions: ['browser', 'development'],
-  },
+  // nodeResolve: {
+  //   exportConditions: ['browser', 'development'],
+  // },
 
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto'
