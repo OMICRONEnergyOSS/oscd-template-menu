cb83cf351170525000d89dc2777412c6df1089cc
Stephen Renwick <stephen.renwick@omicronenergy.com>
2025-08-12 16:27:49 +0200

    fix: copy demo assets to bundle output

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
+import copy from 'rollup-plugin-copy';
 import fs from 'fs';
 
 const tsconfig = JSON.parse(fs.readFileSync('./tsconfig.json', 'utf8'));
@@ -21,7 +22,7 @@ export default [
       sourcemap: true, // Add source map to build output
       format: 'es', // ES module type export
       dir: 'dist', // The build output folder
-      // preserveModules: true,  // Keep directory structure and files
+      // preserveModules: true, // Keep directory structure and files
     },
     preserveEntrySignatures: 'strict', // leaves export of the plugin entry point
 
@@ -49,6 +50,15 @@ export default [
 
       /** Bundle assets references via import.meta.url */
       importMetaAssets(),
+      copy({
+        targets: [
+          { src: 'demo/sample.scd', dest: 'dist/demo' },
+          { src: 'demo/*.js', dest: 'dist/demo' },
+          // Add more patterns if you have more assets
+        ],
+        verbose: true,
+        flatten: false,
+      }),
     ],
     output: {
       dir: 'dist/demo',
